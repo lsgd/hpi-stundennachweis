54698c941d65f38d54f39b9eecc9a2c918f73373
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2015-02-05 11:57:58 +0100

    add google analytics

--- js/base.js
+++ js/base.js
@@ -8,6 +8,17 @@ std.init = function() {
     $('#form-data').submit(std.submitForm);
     $('#form-start').val(new Date().getMonth() + 1);
     $('#year').text(new Date().getFullYear());
+    std.googleAnalytics();
+}
+
+std.googleAnalytics = function() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-36797405-4', 'auto');
+    ga('send', 'pageview');
 }
 
 std.calculateQuartersToHours = function(restHours, distribution, output) {
